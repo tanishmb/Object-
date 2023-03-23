3c170b5116b1584edd5ac773dd4db2f16166e458
tanishmb <96906108+tanishmb@users.noreply.github.com>
2023-03-23 20:25:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+song="";
+song2=""
+
+
+function setup(){
+canvas=createCanvas(500,400);
+ canvas.center();
+  video=createCapture(VIDEO);
+    video.hide();
+    
+   
+}
+
+function draw(){
+    image(video,0,0,500,400);
+}
+function play(){
+    song.play()
+}
+
+function preload(){
+    song=loadSound("Care_Ni_Karda.mp4")
+    song2=loadSound("Mitti_De_Tibbe")
+}
+
+
